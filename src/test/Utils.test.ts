69a0cedd55b6d977265699af1d4de51e5736fe02
Guido <38866056+guidoperman1986@users.noreply.github.com>
2023-09-27 08:47:06 -0300

    basic tests

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,47 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 describe('Utils test suite', () => { 
     test('should return uppercase', () => { 
         const result = toUpperCase('abc')
 
         expect(result).toBe('ABC')
+    })
+
+    describe('To UpperCase examples', () => { 
+        test.each([
+            { input: 'abc', expected: 'ABC',},
+            { input: 'My String', expected: 'MY STRING'},
+            { input: 'def', expected: 'DEF'}])
+        ('$input toUpperCase should be $expected', ({input, expected}) => {
+            const actual = toUpperCase(input)
+            expect(actual).toBe(expected)
+        })
+    })    
+
+    describe("getStringInfo for arg My String should", () => {
+        let actual;
+        beforeEach(()=>{
+            actual = getStringInfo('My String');
+        })
+
+        test('return right length', () => {
+            expect(actual.characters.length).toBe(9)
+            expect(actual.characters).toHaveLength(9)
+        })
+
+        test('return rigth lower case', () => {
+            expect(actual.lowerCase).toBe('my string')
+        })
+
+        test('should first', () => { 
+            expect(actual.characters).toContain<string>('M')
+            
+        })
+
+        test('return extra info', () => { 
+            expect(actual.extraInfo).not.toBe(undefined)
+            expect(actual.extraInfo).not.toBeUndefined()
+            expect(actual.extraInfo).toBeDefined()
+            expect(actual.extraInfo).toBeTruthy()    
+        })
     })
 })
